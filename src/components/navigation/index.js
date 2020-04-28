c98155931ca29495f9dcbab6a9285ea06e89ad84
whoisnian <zhuchangbao1998@gmail.com>
2020-04-28 20:40:21 +0800

    feat: 添加可复用的侧边Drawer导航

---
+++ src/components/navigation/index.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Drawer,
+  List,
+  Link,
+  Divider,
+  ListItem,
+  ListItemText
+} from '@material-ui/core'
+import { PackageVersion } from '../../utils'
+
+const drawerWidth = 240
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex'
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth
+  },
+  toolbar: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'flex-start'
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: drawerWidth
+  },
+  placeholderToolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3)
+  }
+}))
+
+export default function Navigation (props) {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.root}>
+      <AppBar position='fixed' className={classes.appBar}>
+        <Toolbar>
+          <Typography variant='h6'>
+            {props.title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant='permanent'
+        anchor='left'
+        className={classes.drawer}
+        classes={{ paper: classes.drawerPaper }}
+      >
+        <Toolbar className={classes.toolbar}>
+          <Link color='inherit' variant='h6' href='/'>
+            aMonitor-board
+          </Link>
+          <Link color='textSecondary' variant='caption' href='https://github.com/whoisnian/aMonitor-board'>
+            {'v' + PackageVersion}
+          </Link>
+        </Toolbar>
+        <Divider />
+        <List>
+          <ListItem button component='a' href='/overview'>
+            <ListItemText primary='概览' />
+          </ListItem>
+        </List>
+        <Divider />
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.placeholderToolbar} />
+        {props.children}
+      </main>
+    </div>
+  )
+}
