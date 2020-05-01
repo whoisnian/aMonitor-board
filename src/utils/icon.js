7eacb9adef99a584839cd6b521f79125057d377b
whoisnian <zhuchangbao1998@gmail.com>
2020-05-01 21:40:30 +0800

    feat: 加入来自Papirus的linux发行版logo

---
+++ src/utils/icon.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Svgalpine = () => <img src='/img/alpine.svg' />
+const Svgarchlinux = () => <img src='/img/archlinux.svg' />
+const Svgcentos = () => <img src='/img/centos.svg' />
+const Svgdebian = () => <img src='/img/debian.svg' />
+const Svgdeepin = () => <img src='/img/deepin.svg' />
+const Svgfedora = () => <img src='/img/fedora.svg' />
+const Svgfreebsd = () => <img src='/img/freebsd.svg' />
+const Svggentoo = () => <img src='/img/gentoo.svg' />
+const Svgkali = () => <img src='/img/kali.svg' />
+const Svgmanjaro = () => <img src='/img/manjaro.svg' />
+const Svgmint = () => <img src='/img/mint.svg' />
+const Svgopensuse = () => <img src='/img/opensuse.svg' />
+const Svgpuppy = () => <img src='/img/puppy.svg' />
+const Svgraspbian = () => <img src='/img/raspbian.svg' />
+const Svgrhel = () => <img src='/img/rhel.svg' />
+const Svgtux = () => <img src='/img/tux.svg' />
+const Svgubuntu = () => <img src='/img/ubuntu.svg' />
+
+export {
+  Svgalpine,
+  Svgarchlinux,
+  Svgcentos,
+  Svgdebian,
+  Svgdeepin,
+  Svgfedora,
+  Svgfreebsd,
+  Svggentoo,
+  Svgkali,
+  Svgmanjaro,
+  Svgmint,
+  Svgopensuse,
+  Svgpuppy,
+  Svgraspbian,
+  Svgrhel,
+  Svgtux,
+  Svgubuntu
+}
