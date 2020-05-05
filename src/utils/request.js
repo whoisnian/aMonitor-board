047d1fe8f6ba8bbdb70e3daa3cae2fc87bfd67d6
whoisnian <zhuchangbao1998@gmail.com>
2020-05-05 11:21:31 +0800

    feat: 增加delete和put请求封装

--- src/utils/request.js
+++ src/utils/request.js
@@ -16,6 +16,16 @@ const fetchPostHead = async (url, data) => {
   if (!result.ok) throw new Error(`[fetchPostHead] failed with ${result.status}: ${url}`)
 }
 
+const fetchDeleteHead = async (url, data) => {
+  const result = await window.fetch(url, { method: 'DELETE', body: JSON.stringify(data), ...FETCH_OPTIONS })
+  if (!result.ok) throw new Error(`[fetchDeleteHead] failed with ${result.status}: ${url}`)
+}
+
+const fetchPutHead = async (url, data) => {
+  const result = await window.fetch(url, { method: 'PUT', body: JSON.stringify(data), ...FETCH_OPTIONS })
+  if (!result.ok) throw new Error(`[fetchPutHead] failed with ${result.status}: ${url}`)
+}
+
 const fetchGetJSON = async (url) => {
   const result = await window.fetch(url, { method: 'GET', ...FETCH_OPTIONS })
   if (!result.ok) throw new Error(`[fetchPostJSON] failed with ${result.status}: ${url}`)
@@ -25,6 +35,18 @@ const fetchGetJSON = async (url) => {
 const fetchPostJSON = async (url, data) => {
   const result = await window.fetch(url, { method: 'POST', body: JSON.stringify(data), ...FETCH_OPTIONS })
   if (!result.ok) throw new Error(`[fetchPostJSON] failed with ${result.status}: ${url}`)
+  return result.json()
+}
+
+const fetchDeleteJSON = async (url, data) => {
+  const result = await window.fetch(url, { method: 'DELETE', body: JSON.stringify(data), ...FETCH_OPTIONS })
+  if (!result.ok) throw new Error(`[fetchDeleteJSON] failed with ${result.status}: ${url}`)
+  return result.json()
+}
+
+const fetchPutJSON = async (url, data) => {
+  const result = await window.fetch(url, { method: 'PUT', body: JSON.stringify(data), ...FETCH_OPTIONS })
+  if (!result.ok) throw new Error(`[fetchPutJSON] failed with ${result.status}: ${url}`)
   return result.json()
 }
 
@@ -46,11 +68,35 @@ const fetchPostJSONWithStatus = async (url, data) => {
   }
 }
 
+const fetchDeleteJSONWithStatus = async (url, data) => {
+  const result = await window.fetch(url, { method: 'DELETE', body: JSON.stringify(data), ...FETCH_OPTIONS })
+  return {
+    ok: result.ok,
+    status: result.status,
+    content: await result.json()
+  }
+}
+
+const fetchPutJSONWithStatus = async (url, data) => {
+  const result = await window.fetch(url, { method: 'PUT', body: JSON.stringify(data), ...FETCH_OPTIONS })
+  return {
+    ok: result.ok,
+    status: result.status,
+    content: await result.json()
+  }
+}
+
 export {
   fetchGetHead,
   fetchPostHead,
+  fetchDeleteHead,
+  fetchPutHead,
   fetchGetJSON,
   fetchPostJSON,
+  fetchDeleteJSON,
+  fetchPutJSON,
   fetchGetJSONWithStatus,
-  fetchPostJSONWithStatus
+  fetchPostJSONWithStatus,
+  fetchDeleteJSONWithStatus,
+  fetchPutJSONWithStatus
 }
