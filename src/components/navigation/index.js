95cc6a0ba3803f54fbcbf3f40c7a03d884498f35
whoisnian <zhuchangbao1998@gmail.com>
2020-05-13 21:04:34 +0800

    fix: 屏幕过窄时隐藏侧边抽屉

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -7,10 +7,13 @@ import {
   Drawer,
   List,
   Link,
+  IconButton,
+  Hidden,
   Divider,
   ListItem,
   ListItemText
 } from '@material-ui/core'
+import { Menu } from '@material-ui/icons'
 import { PackageVersion } from '../../utils'
 
 const drawerWidth = 240
@@ -20,8 +23,16 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex'
   },
   appBar: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth
+    }
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none'
+    }
   },
   toolbar: {
     display: 'flex',
@@ -30,8 +41,10 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'flex-start'
   },
   drawer: {
-    width: drawerWidth,
-    flexShrink: 0
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0
+    }
   },
   drawerPaper: {
     width: drawerWidth
@@ -46,52 +59,90 @@ const useStyles = makeStyles((theme) => ({
 export default function Navigation (props) {
   const classes = useStyles()
 
+  const [mobileOpen, setMobileOpen] = React.useState(false)
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen)
+  }
+
+  const drawer = (
+    <div>
+      <Toolbar className={classes.toolbar}>
+        <Link color='inherit' variant='h6' href='/'>
+            aMonitor-board
+        </Link>
+        <Link color='textSecondary' variant='caption' href='https://github.com/whoisnian/aMonitor-board'>
+          {'v' + PackageVersion}
+        </Link>
+      </Toolbar>
+      <Divider />
+      <List>
+        <ListItem button component='a' href='/overview'>
+          <ListItemText primary='概览' />
+        </ListItem>
+        <ListItem button component='a' href='/agents'>
+          <ListItemText primary='主机列表' />
+        </ListItem>
+        <ListItem button component='a' href='/groups'>
+          <ListItemText primary='规则管理' />
+        </ListItem>
+        <ListItem button component='a' href='/receivers'>
+          <ListItemText primary='推送管理' />
+        </ListItem>
+        <ListItem button component='a' href='/messages'>
+          <ListItemText primary='警报历史' />
+        </ListItem>
+        <ListItem button component='a' href='/preferences'>
+          <ListItemText primary='系统设置' />
+        </ListItem>
+      </List>
+      <Divider />
+    </div>
+  )
+
+  const container = window !== undefined ? () => window.document.body : undefined
+
   return (
     <div className={classes.root}>
       <AppBar position='fixed' className={classes.appBar}>
         <Toolbar>
+          <IconButton
+            color='inherit'
+            edge='start'
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <Menu />
+          </IconButton>
           <Typography variant='h6'>
             {props.title}
           </Typography>
         </Toolbar>
       </AppBar>
-      <Drawer
-        variant='permanent'
-        anchor='left'
-        className={classes.drawer}
-        classes={{ paper: classes.drawerPaper }}
-      >
-        <Toolbar className={classes.toolbar}>
-          <Link color='inherit' variant='h6' href='/'>
-            aMonitor-board
-          </Link>
-          <Link color='textSecondary' variant='caption' href='https://github.com/whoisnian/aMonitor-board'>
-            {'v' + PackageVersion}
-          </Link>
-        </Toolbar>
-        <Divider />
-        <List>
-          <ListItem button component='a' href='/overview'>
-            <ListItemText primary='概览' />
-          </ListItem>
-          <ListItem button component='a' href='/agents'>
-            <ListItemText primary='主机列表' />
-          </ListItem>
-          <ListItem button component='a' href='/groups'>
-            <ListItemText primary='规则管理' />
-          </ListItem>
-          <ListItem button component='a' href='/receivers'>
-            <ListItemText primary='推送管理' />
-          </ListItem>
-          <ListItem button component='a' href='/messages'>
-            <ListItemText primary='警报历史' />
-          </ListItem>
-          <ListItem button component='a' href='/preferences'>
-            <ListItemText primary='系统设置' />
-          </ListItem>
-        </List>
-        <Divider />
-      </Drawer>
+      <nav className={classes.drawer}>
+        <Hidden smUp implementation='css'>
+          <Drawer
+            container={container}
+            variant='temporary'
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{ paper: classes.drawerPaper }}
+            ModalProps={{ keepMounted: true }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation='css'>
+          <Drawer
+            classes={{ paper: classes.drawerPaper }}
+            variant='permanent'
+            anchor='left'
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
       <main className={classes.content}>
         <div className={classes.placeholderToolbar} />
         {props.children}
